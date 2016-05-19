b31cffa7aa343a960f021051b50802d722c9e63f
Joao Neto <jlneto@ac.upc.edu>
2016-05-19 12:35:24 +0200

    added import for org.junit

--- src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,8 +1,9 @@
 package io.github.joaomlneto.travis_ci_tutorial_java;
 
-import static org.junit.Assert.*;
+import java.util.*;
 
-import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class SimpleCalculatorTest {
 	@Test
