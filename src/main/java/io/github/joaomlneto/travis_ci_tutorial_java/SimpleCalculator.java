84ce2d391d28ddd78a51cc6a1f44ac125f701947
Joao Neto <jlneto@ac.upc.edu>
2016-05-19 12:21:20 +0200

    added sample test code

---
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -0,0 +1,16 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+public class SimpleCalculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int a, int b) {
+		return a - b;
+	}
+	public int mul(int a, int b) {
+		return a * b;
+	}
+	public int div(int a, int b) {
+		return a / b;
+	}
+}
---
+++ src/test/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -0,0 +1,33 @@
+package io.github.joaomlneto.travis_ci_tutorial_java;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleCalculatorTest {
+	@Test
+	public void testAdd() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(1, 1), 2);
+	}
+	@Test
+	public void testSub() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.sub(1, 1), 0);
+	}
+	@Test
+	public void testMul() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mul(3, 5), 15);
+	}
+	@Test
+	public void testDiv() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.div(8, 2), 4);
+	}
+	@Test
+	public void testAddBad() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.add(1, 1), 3);
+	}
+}
