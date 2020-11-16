00b45cb96c6ff6675b5b360ef306389d41945305
Restyled.io <commits@restyled.io>
2020-11-16 10:38:45 +0000

    Restyled by clang-format

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,19 +1,11 @@
 package io.github.joaomlneto.travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
-		return a + b;
-	}
-	public int sub(int a, int b) {
-		return a - b;
-	}
-	public int mul(int a, int b) {
-		return a * b;
-	}
-	public int div(int a, int b) {
-		return a / b;
-	}
-	public static final void main(String[] args) {
-		System.out.println("Hello world test1?");
-	}
+  public int add(int a, int b) { return a + b; }
+  public int sub(int a, int b) { return a - b; }
+  public int mul(int a, int b) { return a * b; }
+  public int div(int a, int b) { return a / b; }
+  public static final void main(String[] args) {
+    System.out.println("Hello world test1?");
+  }
 }
